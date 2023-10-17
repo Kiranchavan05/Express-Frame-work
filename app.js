85c5c66b5eb3952501f0d298c35c3eb6e57395a4
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-10-17 13:37:14 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express=require('express')
-const bodyParse=require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser')
 
+const app = express()
 
-const app=express();
+const path = require('path')
 
-app.use(bodyParse.urlencoded({extended:false}))
+const adminroutes = require('./routes/admin')
+const shoproutes = require('./routes/shop')
+const contactRoutes = require('./routes/contactus')
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/admin',adminroutes)
+app.use(shoproutes)
+app.use(contactRoutes)
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use((req,res,next) => {
+         res.status(404).sendFile(path.join(__dirname,'views','404.html'))   
 })
 
-
-app.post('/product', (req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express!!!</h1>')
-})
 
 app.listen(4000);
